6f6a25866d116af221d05aa2a1541075952566e7
Julian Barbosa <bjvlixn@gmail.com>
2021-10-12 02:03:04 -0300

    add loader's style

--- src/Components/Helper/style/Loading.js
+++ src/Components/Helper/style/Loading.js
@@ -1,1 +1,22 @@
 import styled from "styled-components";
+
+export const wrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+`;
+
+export const loading = styled.div`
+  margin: auto;
+  width: 80px;
+  height: 80px;
+  display: grid;
+  place-items: center;
+  border-radius: 50%;
+  padding-left: 5px;
+  background-color: rgba(255, 255, 255, 0.5);
+`;
