3471619f2acc1e3cb462270a7db81a82839fc594
Julian Barbosa <bjvlixn@gmail.com>
2021-10-22 00:00:15 -0300

    added header options to PHOTO_GET function in api file

--- src/api.js
+++ src/api.js
@@ -76,6 +76,10 @@ export const PHOTOS_GET = ({ page, total, user }) => {
 export const PHOTO_GET = (id) => {
   return {
     url: `${API_URL}/api/photo/${id}`,
+    options: {
+      method: "GET",
+      cache: "no-store",
+    },
   };
 };
 
