ca06335180219ee3026a31d00e92cde3c0e55576
Julian Barbosa <bjvlixn@gmail.com>
2021-10-29 23:54:14 -0300

    add Head component

---
+++ src/Components/Helper/Head.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+const Head = (props) => {
+  useEffect(() => {
+    document.title = `${props.title} | Dogs`;
+    document
+      .querySelector("meta[name='description']")
+      .setAttribute("content", props.description || "");
+  }, [props]);
+
+  return <div></div>;
+};
+
+export default Head;
