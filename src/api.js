98793b7b35e40179cf670fcd6bb68c5c8d93fedd
Julian Barbosa <bjvlixn@gmail.com>
2021-11-04 00:05:12 -0300

    add api call, new package to Graph manipulation

--- src/api.js
+++ src/api.js
@@ -135,3 +135,15 @@ export const PASSWORD_RESET = (body) => {
     },
   };
 };
+
+export const STATS_GET = () => {
+  return {
+    url: `${API_URL}/api/stats`,
+    options: {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      },
+    },
+  };
+};
